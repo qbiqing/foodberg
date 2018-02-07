461454f13f50f3f3d5e2762a31a7714f66c6dc27
qbiqing <bq2134@columbia.edu>
2018-02-06 22:46:07 -0500

    added nav bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './media/logo.svg';
+import { Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Nav from './components/Nav';
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav/>
+        <Switch>
+          <Route exact path = '/' component={Home}/>
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+
+class Home extends Component{
+	render(){
+		return(
+      <div className="home">
+      	<Nav/>
+        <header className="App-header">
+          
+        </header>
+        <p className="App-intro">
+        foooodberg
+
+
+
+
+
+
+
+
+        
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
